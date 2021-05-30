1753f1f89c5af92a596a646cad6878721ff15e29
Kartik0899 <72159742+Kartik0899@users.noreply.github.com>
2021-05-30 20:24:12 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import { ReactComponent as CloseMenu } from "./assets/x.svg";
+import { ReactComponent as MenuIcon } from "./assets/menu.svg";
+import "./Header.css";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const Header = () => {
+  const [click, setClick] = useState(false);
+  const [isNext, setIsNext] = useState(true);
+  const [pageno, setPageno] = useState(1);
+  const [books, setBooks] = useState([]);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const fetchData = async () => {
+    const res = await fetch(`https://reqres.in/api/users?page=${pageno}`);
+    const json = await res.json();
+    setBooks(json.data);
+  };
+
+  const fetchMoreData = () => {
+    fetchData();
+    if (pageno === 2) {
+      setIsNext(false);
+    }
+    setTimeout(setPageno(pageno + 1), 1000);
+  };
+  return (
+    <>
+      <div className="header">
+        <div className="logo-nav">
+          <div className="logo-container">
+            <a href="\" className="">
+              LOGO
+            </a>
+          </div>
+          <ul className={click ? "nav-options active" : "nav-options"}>
+            <li className="option" onClick={closeMobileMenu}>
+              <a href="\">ABOUT</a>
+            </li>
+            <li className="option" onClick={closeMobileMenu}>
+              <a href="\">CONTACT</a>
+            </li>
+            <li className="option" onClick={closeMobileMenu}>
+              <a href="\">BLOG</a>
+            </li>
+            <li className="option mobile-option" onClick={closeMobileMenu}>
+              <a
+                className="btns"
+                onClick={fetchMoreData}
+                onDoubleClick={() => setPageno(1)}
+              >
+                Fetch Data
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <ul className="signin-up">
+          <li onClick={closeMobileMenu}>
+            <a
+              className="btns btnss"
+              onClick={fetchMoreData}
+              onDoubleClick={() => setPageno(1)}
+            >
+              Fetch Data
+            </a>
+          </li>
+        </ul>
+        <div className="mobile-menu" onClick={handleClick}>
+          {click ? (
+            <CloseMenu className="menu-icon" />
+          ) : (
+            <MenuIcon className="menu-icon" />
+          )}
+        </div>
+      </div>
+      <InfiniteScroll
+        dataLength={books.length}
+        next={() => {
+          setTimeout(() => {
+            fetchMoreData();
+          }, 1000);
+        }}
+        hasMore={isNext}
+        loader={
+          <div className="centerload">
+            <h4 className="load loader">Loading</h4>
+          </div>
+        }
+        endMessage={
+          <p className="load">
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <div className="books">
+          {books.map((book) => {
+            return (
+              <div className="book" key={book.id}>
+                <div className="details">
+                  <p>{book.id}</p>
+                  <p>{book.first_name}</p>
+                  <p>{book.last_name}</p>
+                  <p> {book.email}</p>
+                  <img key={book.avatar} src={book.avatar} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
+
+export default Header;
